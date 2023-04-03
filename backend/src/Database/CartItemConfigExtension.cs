0e4dccfcd84e1f02501184e9953d3c90a29a6b8c
Nhu Nguyen Tran Binh <binhnhu1409@gmail.com>
2023-04-03 12:18:40 +0300

    backend: added composite key for CartItem as suggested

--- backend/src/Database/CartItemConfigExtension.cs
+++ backend/src/Database/CartItemConfigExtension.cs
@@ -7,17 +7,6 @@ public static class CartItemConfigExtension
 {
     public static void AddCartItemConfig(this ModelBuilder builder) 
     {
-        builder.Entity<CartItem>(entity => {
-            entity
-                .HasOne(e => e.Product)
-                .WithMany()
-                .HasForeignKey(e => e.ProductId)
-                .OnDelete(DeleteBehavior.Cascade);
-            entity
-                .HasOne(e => e.Cart)
-                .WithMany()
-                .HasForeignKey(e => e.CartId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
+        builder.Entity<CartItem>().HasKey(ci => new { ci.ProductId, ci.CartId});
     }
 }
