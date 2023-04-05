c7985afcb34804bc40187cacb1537ab85f455a11
binhnhu1409 <binhnhu1409@gmail.com>
2023-04-05 23:10:58 +0300

    backend: remove Address Model

--- backend/src/Models/Address.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.src.Models;
-
-public class Address : BaseModel
-{
-    public string Street { get; set; } = null!;
-    public string City { get; set; } = null!;
-    public string Zipcode { get; set; } = null!;
-    public string Country { get; set; } = null!;
-}
